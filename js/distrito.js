cbb1e5587a1a5fd81e430e435d3d75b59478d564
root <henryalejo@gmail.com>
2015-02-27 18:32:41 -0500

    cambio de array

--- js/distrito.js
+++ js/distrito.js
@@ -1,38 +1,37 @@
-///<reference path="../extern/jquery.d.ts"/>
 var tmdb;
 (function (tmdb) {
     //Declaracion de el objeto NodeType y sus prototipos
     var nodoOlinguito=
         {
-            "id": 1,
+            "id": "1763628",
             "type": 1,
             "name": "Olinguito",
             "image": "https://avatars2.githubusercontent.com/u/8237020?v=3&s=100",
             "relations": [
                 {
-                    "id": 2
+                    "id": 2341312
                 },
                 {
-                    "id": 3
+                    "id": 3567564
                 }
             ],
             "article": false
         };
 var nodoOvalle={
-            "id": 2,
+            "id": "2341312",
             "type": 2,
             "name": "Ovalle",
             "image": "https://avatars1.githubusercontent.com/u/6756995?v=3&s=40",
             "relations": [
                 
                 {
-                    "id": 9
+                    "id": "9645747"
                 },
                 {
-                    "id": 10
+                    "id": "1034578"
                 },
                 {
-                    "id": 8
+                    "id": "8213623"
                 }
             ],
             "article": false
@@ -41,28 +40,28 @@ var nodoOvalle={
 
 
 var nodoRoberto={
-            "id": 3,
+            "id": "3567564",
             "type": 2,
             "name": "Roberto",
             "image": "https://avatars2.githubusercontent.com/u/7140772?v=3&s=40",
             "relations": [
                 {
-                    "id": 8
+                    "id": "8213623"
                 },
                 {
-                    "id": 9
+                    "id": "9645747"
                 },
                 {
-                    "id": 10
+                    "id": "1034578"
                 },
                 {
-                    "id": 11
+                    "id": "1135673"
                 }
             ],
             "article": false
         };
     var nodoOcho={
-            "id": 8,
+            "id": "8213623",
             "type": 2,
             "name": "Ocho",
             "image": "https://avatars1.githubusercontent.com/u/6756996?v=3&s=40",
@@ -72,7 +71,7 @@ var nodoRoberto={
             "article": false
         };
     var nodoNueve={
-            "id": 9,
+            "id": "9645747",
             "type": 2,
             "name": "Nueve",
             "image": "https://avatars1.githubusercontent.com/u/6756997?v=3&s=40",
@@ -82,7 +81,7 @@ var nodoRoberto={
             "article": false
         };
     var nodoDiez={
-        "id": 10,
+        "id": "1034578",
         "type": 2,
         "name": "Diez",
         "image": "https://avatars1.githubusercontent.com/u/6756998?v=3&s=40",
@@ -92,7 +91,7 @@ var nodoRoberto={
         "article": false
     };
     var nodoOnce={
-        "id": 11,
+        "id": "1135673",
         "type": 2,
         "name": "Once",
         "image": "https://avatars1.githubusercontent.com/u/6756999?v=3&s=40",
@@ -101,11 +100,11 @@ var nodoRoberto={
         ],
         "article": false
     };
-    var misnodos =[nodoOvalle,nodoOlinguito,nodoRoberto,nodoOcho,nodoNueve,nodoDiez,nodoOnce]
+    var misnodos =new Array(nodoOvalle,nodoOlinguito,nodoRoberto,nodoOcho,nodoNueve,nodoDiez,nodoOnce);
    
     var NodeType2 = (function () {
-        console.log('entra a 1 NodeType');
-        console.log(misnodos);
+        //console.log('entra a 1 NodeType');
+        //console.log(misnodos);
         function NodeType2(type,image, relations, article, instruments, influences) {
             this.type = type;
             this.image =image;
@@ -129,7 +128,7 @@ var nodoRoberto={
     })();
     
     tmdb.NodeType2 = NodeType2;
-    //console.log('entra a 2');
+    ////console.log('entra a 2');
     //Creacion de dos tipos de nodo Movie and Person
     //tmdb.Movie = new NodeType2("movie", "credits", "title", "posters");
     //tmdb.Person = new NodeType2("person", "movie_credits", "name", "profiles");
@@ -138,7 +137,7 @@ var nodoRoberto={
     //Declaracion de el objeto Nodo y sus  prototipos
    
     var Node2 = (function () {
-        console.log('entra a 2 Node');
+        //console.log('entra a 2 Node');
         function Node2(id, type) {
             this.info = request2(id)[0];           
             this.type = type;
@@ -147,14 +146,14 @@ var nodoRoberto={
 
         }
         Node2.prototype.name = function () {
-            //console.log(this.type + this.id.toString());
+            ////console.log(this.type + this.id.toString());
             return 'tag'+this.id; // Este es el nombre del nodo
         };
         Node2.prototype.getImage = function () {
 
-            //console.log(this.info);
-            //console.log(this.id);
-            //console.log(this.info.image);
+            ////console.log(this.info);
+            ////console.log(this.id);
+            ////console.log(this.info.image);
             //return $.get(this.info.image);
 
             var _this = this;
@@ -183,9 +182,9 @@ var nodoRoberto={
    
 
     var Edge2 = (function () {
-        console.log('entra a 3 Crea Edge2');
+        //console.log('entra a 3 Crea Edge2');
         function Edge2(source, target) {
-            console.log('edge2');
+            //console.log('edge2');
             this.source = source;
             this.target = target;
         }
@@ -198,7 +197,7 @@ var nodoRoberto={
     tmdb.Edge2 = Edge2;
     //funcion que hace el llamado al api de peliculas y trae la informacion
     function request(type, id, content, append) {
-        console.log('entra a request con: '+type + '   '+id +'   '+content + '   '+append);
+        //console.log('entra a request con: '+type + '   '+id +'   '+content + '   '+append);
         if (typeof content === "undefined") { content = null; }
         if (typeof append === "undefined") { append = null; }
         var query = "https://api.themoviedb.org/3/" + type + "/" + id;
@@ -209,12 +208,12 @@ var nodoRoberto={
         if (append) {
             query += "&append_to_response=" + append;
         }
-        //console.log($.get(query));
+        ////console.log($.get(query));
         return $.get(query);
     }
 
      function requestSons(sons) { 
-        console.log(sons);
+        //console.log(sons);
         var nodesons =[];
         for (var i = misnodos.length - 1; i >= 0; i--) {
             
@@ -229,16 +228,16 @@ var nodoRoberto={
     function request2(id) {
         var mynode =[];
         for (var i = misnodos.length - 1; i >= 0; i--) {
-                //console.log('for'+ i + misnodos[i].id + '='+id);
+                ////console.log('for'+ i + misnodos[i].id + '='+id);
             if (misnodos[i].id==id){
-                //console.log('entro al if del request');
+                ////console.log('entro al if del request');
                     mynode.push(misnodos[i]);
                     break;
                 };
             
         };
-        console.log('entro al request');
-        console.log(mynode);
+        //console.log('entro al request');
+        //console.log(mynode);
 
         return mynode;
     };
@@ -246,14 +245,14 @@ var nodoRoberto={
     
 
     var Graph2 = (function () {
-        console.log('entra a  5 Graph');
+        //console.log('entra a  5 Graph');
         function Graph2() {
             this.nodes = {};
             this.edges = {};
         }
         Graph2.prototype.expandNeighbours = function (node, f) {
-            console.log('entra a expande vecinos');
-            console.log('expandiendo vecinos:'+node +' '+f)
+            //console.log('entra a expande vecinos');
+            //console.log('expandiendo vecinos:'+node +' '+f)
             var _this = this;
             /*
             var dn = node.cast.map(function (c) {
@@ -283,38 +282,38 @@ var nodoRoberto={
             return d.promise();
         };
         Graph2.prototype.fullyExpanded = function (node) {
-            console.log('entra a fullyExpanded');
+            //console.log('entra a fullyExpanded');
             var _this = this;
             return node.cast && node.cast.every(function (v) {
                 return ( v.id) in _this.nodes;
             });
         };
         Graph2.prototype.addNode = function (id, type) {
-            console.log('entra adiciona Nodo');
+            //console.log('entra adiciona Nodo');
             var node = new Node2(id, type);
             return this.nodes[node.name()] = node;
         };
         Graph2.prototype.getNode = function (id, type, f) {//aqui vamos
-            console.log('entra aget node');
+            //console.log('entra aget node');
             var _this = this;
             var d = $.Deferred();
-            console.log('_This nodes en get node:');
-            console.log(_this.nodes);
+            //console.log('_This nodes en get node:');
+            //console.log(_this.nodes);
             //var name = type + id.toString();
             var name = 'tag'+ id.toString();
-            console.log('_This nodes en get node:');
-            console.log(_this.nodes);
+            //console.log('_This nodes en get node:');
+            //console.log(_this.nodes);
 
             if (name in this.nodes) {
                 return this.nodes[name];
             }
             var node = this.addNode(id, type);
             f(node);
-            console.log('NOdo antes del cast');
-            console.log(node);
+            //console.log('NOdo antes del cast');
+            //console.log(node);
             var cast = requestSons(node.info.relations);
-            console.log('El cast es:');
-            console.log(cast);
+            //console.log('El cast es:');
+            //console.log(cast);
 
             $.when(cast).then(function (c) {
                 node.label = node.info.name;
@@ -329,7 +328,7 @@ var nodoRoberto={
             return d.promise();
         };
         Graph2.prototype.addEdge = function (u, v) {
-            console.log('entra adiciona edge');
+            //console.log('entra adiciona edge');
             var edge = u.makeEdge(u.name(), v.name());
             var ename = edge.toString();
             if (!(ename in this.edges)) {
@@ -351,4 +350,3 @@ var nodoRoberto={
 
 
 })(tmdb || (tmdb = {}));
-//# sourceMappingURL=tmdbgraph.js.map
--- js/tmdbgraph.js
+++ js/tmdbgraph.js
@@ -3,7 +3,7 @@ var tmdb;
 (function (tmdb) {
     //Declaracion de el objeto NodeType y sus prototipos
     var NodeType = (function () {
-    	console.log('entra a 1');
+    	//console.log('entra a 1');
         function NodeType(type, credits, label, imagesarray) {
             this.type = type;
             this.credits = credits;
@@ -24,20 +24,20 @@ var tmdb;
         return NodeType;
     })();
     tmdb.NodeType = NodeType;
-	console.log('entra a 2');
+	//console.log('entra a 2');
     //Creacion de dos tipos de nodo Movie and Person
     tmdb.Movie = new NodeType("movie", "credits", "title", "posters");
     tmdb.Person = new NodeType("person", "movie_credits", "name", "profiles");
     //Declaracion de el objeto Nodo y sus  prototipos
     var Node = (function () {
-    	console.log('entra a 3');
+    	//console.log('entra a 3');
         function Node(type, id) {
             this.type = type;
             this.id = id;
             this.degree = 0;
         }
         Node.prototype.name = function () {
-        	//console.log(this.type + this.id.toString());
+        	////console.log(this.type + this.id.toString());
             return this.type + this.id.toString(); // Este es el nombre del nodo
         };
         Node.prototype.getImage = function () {
@@ -48,7 +48,7 @@ var tmdb;
                 var paths = i[_this.type.imagesarray];
 
                 _this.imgurl = paths.length > 0 ? 'http://image.tmdb.org/t/p/w185/' + paths[0].file_path : 'http://upload.wikimedia.org/wikipedia/commons/3/37/No_person.jpg';
-               // console.log('Imagen con index: '+  _this.imgurl);
+               // //console.log('Imagen con index: '+  _this.imgurl);
                 d.resolve(_this);
             });
 
@@ -59,9 +59,9 @@ var tmdb;
     tmdb.Node = Node;
     //Declaracion de el objeto Edge y sus prototipos
     var Edge = (function () {
-    	console.log('entra a 4');
+    	//console.log('entra a 4');
         function Edge(source, target) {
-        	console.log('entra a 5');
+        	//console.log('entra a 5');
             this.source = source;
             this.target = target;
         }
@@ -74,7 +74,7 @@ var tmdb;
     tmdb.Edge = Edge;
     //funcion que hace el yamado al api de peliculas y trae la informacion
     function request(type, id, content, append) {
-    	console.log('entra a A con: '+type + '   '+id +'   '+content + '   '+append);
+    	//console.log('entra a A con: '+type + '   '+id +'   '+content + '   '+append);
         if (typeof content === "undefined") { content = null; }
         if (typeof append === "undefined") { append = null; }
         var query = "https://api.themoviedb.org/3/" + type + "/" + id;
@@ -85,19 +85,19 @@ var tmdb;
         if (append) {
             query += "&append_to_response=" + append;
         }
-        console.log(query);
+        //console.log(query);
         return $.get(query);
     }
     //Se declara la clase Graph donde se declara dos arreglos uno de nodes y uno de edges, ademas se definen unos prototipos
     var Graph = (function () {
-    	console.log('entra a 7');
+    	//console.log('entra a 7');
         function Graph() {
             this.nodes = {};
             this.edges = {};
         }
         Graph.prototype.expandNeighbours = function (node, f) {
-        	console.log('entra a 8');
-        	console.log('expandiendo vecinos:'+node +' '+f)
+        	//console.log('entra a 8');
+        	//console.log('expandiendo vecinos:'+node +' '+f)
             var _this = this;
 
             var dn = node.cast.map(function (c) {
@@ -107,8 +107,8 @@ var tmdb;
                     f(v);
                 });
             });
-            console.log('El dn del cast');
-            console.log(dn);
+            //console.log('El dn del cast');
+            //console.log(dn);
             //No ententi hay que revizar
             var d = $.Deferred();
             $.when.apply($, dn).then(function () {
@@ -118,25 +118,27 @@ var tmdb;
             return d.promise();
         };
         Graph.prototype.fullyExpanded = function (node) {
-        	console.log('entra a 9');
+        	//console.log('entra a 9');
             var _this = this;
             return node.cast && node.cast.every(function (v) {
+                //console.log('entra a nodecast Every');
+                //console.log(node.type.next() + '' +v.id);
                 return (node.type.next() + v.id) in _this.nodes;
             });
         };
         Graph.prototype.addNode = function (type, id) {
-        	console.log('entra a 10');
+        	//console.log('entra a 10');
 
             var node = new Node(type, id);
             return this.nodes[node.name()] = node;
         };
         Graph.prototype.getNode = function (type, id, f) {
-        	console.log('entra a 11');
+        	//console.log('entra a 11');
             
             var _this = this;
             var d = $.Deferred();
-            console.log('_This nodes en get node:');
-            console.log(_this.nodes);
+            //console.log('_This nodes en get node:');
+            //console.log(_this.nodes);
             var name = type + id.toString();
             if (name in this.nodes) {
                 return this.nodes[name];
@@ -147,23 +149,25 @@ var tmdb;
 
             f(node);
             var cast = request(type, id, null, type.credits);
-            console.log('cast:');
-            console.log(cast);
+            //console.log('cast:');
+            //console.log(cast);
             $.when(cast).then(function (c) {
                 node.label = c[type.label];
-                console.log('node.label:');
-                console.log(node.label);
-                console.log('_This nodes  primer cast');
-                 console.log(_this.nodes);
+                //console.log('node.label:');
+                //console.log(node.label);
+                //console.log('_This nodes  primer cast');
+                 //console.log(_this.nodes);
                 (node.cast = c[type.credits].cast).forEach(function (v) {
-                    console.log('node.cast:');
-                    console.log(node.cast);
+                    //console.log('node.cast:');
+                    //console.log(node.cast);
                     var neighbourname = type.next() + v.id.toString();
                     console.log('neighbourname:');
                     console.log(neighbourname);
+                    console.log(_this.nodes);
+                    console.log('El if que no sirve:'  +_this.nodes["person819"] );
                     if (neighbourname in _this.nodes) {
-                        console.log('_This nodes if');
-                        console.log(_this.nodes);
+                        console.log('Entra el if del vecinos:');
+                        console.log(_this.nodes[neighbourname]);
                         _this.addEdge(node, _this.nodes[neighbourname]);
                     }
                 });
@@ -172,7 +176,7 @@ var tmdb;
             return d.promise();
         };
         Graph.prototype.addEdge = function (u, v) {
-        	console.log('entra a 12');
+        	//console.log('entra a 12');
             var edge = u.type.makeEdge(u.name(), v.name());
             var ename = edge.toString();
             if (!(ename in this.edges)) {
